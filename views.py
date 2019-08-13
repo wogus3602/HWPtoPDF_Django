41389578710414d554a14d42ab02514420f6246b
leemyo <52395465+leemyo@users.noreply.github.com>
2019-08-13 17:13:37 +0900

    Add files via upload

---
+++ views.py
@@ -0,0 +1,37 @@
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from subprocess import call
+import pyrebase, json, requests
+
+@csrf_exempt 
+def echo(req):
+	fileUrl = str(req.POST['fileUrl'])
+	config = {
+  		"apiKey": "AIzaSyDCUr8ng_lqfuwHEzOTE-yF2mbarPpBm5M",
+  		"authDomain": "boba-eecca.firebaseapp.com",
+  		"databaseURL": "https://boba-eecca.firebaseio.com",
+  		"storageBucket": "boba-eecca.appspot.com",
+		}
+	firebase = pyrebase.initialize_app(config)
+	auth = firebase.auth()
+	user = auth.sign_in_with_email_and_password('leem@plz.com', 'qweqwe')
+	storage = firebase.storage()
+
+	url=fileUrl.split('?')
+	fileName=url[0].split('/')
+	hwpName=fileName[len(fileName)-1]			#hwpName=lecture.hwp
+	fileN=fileName[len(fileName)-1].split('.')		#fileN[0]=lecture
+	pdfName=fileN[0]+'.pdf'					#pdfName=lecture.pdf
+	storage.child(hwpName).download( hwpName , user['idToken'])
+
+	call('hwp5html '+hwpName, shell=True)			#transformation
+	call('wkhtmltopdf.sh -s A5 ./'+fileN[0]+'/index.xhtml '+pdfName, shell=True)
+	call('rm -rf '+fileN[0]+' '+'hwpName', shell=True)		#remove files in server
+
+	uploadfile = "./"+pdfName
+	storage.child(pdfName).put(uploadfile)
+	
+	fileUrl = str(storage.child(pdfName).get_url(1))		#get pdf's new url
+	call('rm '+pdfName, shell=True)				#remove pdf file in server
+
+	return HttpResponse(fileUrl)
